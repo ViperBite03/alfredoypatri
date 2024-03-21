3b8f31bf9eb1dbc268c937941eb70686785c299c
ViperBite03 <layitamartin@gmail.com>
2024-03-21 14:30:57 +0100

    vercel?

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import db from '@astrojs/db'
 
-import cloudflare from '@astrojs/cloudflare'
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), db()],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: vercel(),
 })
